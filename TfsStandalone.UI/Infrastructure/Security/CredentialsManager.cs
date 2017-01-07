fc52c1b85a18ec1e68c03c76795b76bfea13d703
Robin Home PC <robinherd@hotmail.co.uk>
2017-01-07 16:56:10 +0000

    initial work for caching credentials

---
+++ TfsStandalone.UI/Infrastructure/Security/CredentialsManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace TfsStandalone.UI.Infrastructure.Security
+{
+    using System.Security.Cryptography;
+
+    public class CredentialsManager
+    {
+        private static byte[] _entropy;
+        private static byte[] _encrypted;
+        private static string _username;
+
+        public UserPass RetrieveCredentials()
+        {
+            var plaintext = ProtectedData.Unprotect(_encrypted, _entropy, DataProtectionScope.CurrentUser);
+            return new UserPass { Username = _username, Password = Encoding.UTF8.GetString(plaintext) };
+        }
+
+        public void StoreCredentials(string username, string password)
+        {
+            var plaintext = Encoding.UTF8.GetBytes(password);
+
+            // entropy is optional, but ensures encrypted data is more difficult for other applications to access
+            _entropy = GenerateEntropy();
+            _encrypted = ProtectedData.Protect(plaintext, _entropy, DataProtectionScope.CurrentUser);
+            _username = username;
+        }
+        
+        private byte[] GenerateEntropy()
+        {
+            var entropy = new byte[20];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(entropy);
+            }
+
+            return entropy;
+        }
+    }
+}
---
+++ TfsStandalone.UI/Infrastructure/Security/UserPass.cs
@@ -0,0 +1,8 @@
+﻿namespace TfsStandalone.UI.Infrastructure.Security
+{
+    public class UserPass
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
